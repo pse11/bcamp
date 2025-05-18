dbf92a6bd1657976577db6ba0d0ae9ad7b21dae5
pse11 <seal30290@gmail.com>
2025-05-19 01:09:51 +0900

    first commit

---
+++ CodingTest/JS/index.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input =fs.readFileSync('./example.txt').toString().trim();
+
+let year = parseInt(input);
+
+if(year%4==0&&year%100!=0||year%400==0){
+  console.log("1");
+}else {
+  console.log("0");
+}
