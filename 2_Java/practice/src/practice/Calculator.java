45ace579ba3ea38edff250382fd1ad89ff2ad1a0
park sang eun <seal30290@gmail.com>
2025-06-29 23:29:04 +0900

    네트워크 공부

--- 2_Java/practice/src/practice/Calculator.java
+++ 2_Java/practice/src/practice/Calculator.java
@@ -1,26 +1,33 @@
 package practice;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
 public class Calculator {
-	private int memory;
-	public int getMemory() {
-		return memory;
-	}
-	
-	public void setMemory1(int memory) {
-		this.memory=memory;
-		try{
-			Thread.sleep(2000);
-		}catch(InterruptedException e) {}
-		System.out.println(Thread.currentThread().getName()+":"+this.memory);
-	}
-	
-	public void setMemory2(int memory) {
-		
-			this.memory=memory;
-			try {
-				Thread.sleep(2000);
-			}catch(InterruptedException e) {}
-			System.out.println(Thread.currentThread().getName()+":"+this.memory);
+	public static void main(String[] args) {
+		try {
+			DatagramSocket datagramSocket = new DatagramSocket();
+			String data = "정치";
+			byte[] bytes = data.getBytes("UTF-8");
+			DatagramPacket sendPacket = new DatagramPacket(bytes, bytes.length,new InetSocketAddress("localhost",50001));
+			datagramSocket.send(sendPacket);
+			
+			while(true) {
+				DatagramPacket receivePacket = new DatagramPacket(new byte[1024],1024);
+				datagramSocket.receive(receivePacket);
+				
+				String news = new String(receivePacket.getData(),0,receivePacket.getLength(),"UTF-8");
+				System.out.println(news);
+				
+				if(news.contains("뉴스10")) {
+					break;
+				}
+			}
+			datagramSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
--- 2_Java/practice/src/practice/Test.java
+++ 2_Java/practice/src/practice/Test.java
@@ -1,16 +1,63 @@
 package practice;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+import java.util.Scanner;
+
 public class Test {
+	private static DatagramSocket datagramSocket = null;
 	public static void main(String[] args) {
-		Calculator calculator = new Calculator();
+		System.out.println("--------------------------------");
+		System.out.println("서버를 종료하려면 q또는 Q를 입력하고 ENTER키를 입력");
+		System.out.println("--------------------------------");
 		
-		User1Thread user1Thread = new User1Thread();
-		user1Thread.setCalculator(calculator);
-		user1Thread.start();
+		startServer();
 		
-		User2Thread user2Thread = new User2Thread();
-		user2Thread.setCalculator(calculator);
-		user2Thread.start();  
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String key = sc.nextLine();
+			if(key.toLowerCase().equals("q")) {
+				break;
+			}
+		}
+		sc.close();
+		stopServer();
+	}
+	public static void startServer() {
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					datagramSocket = new DatagramSocket(50001);
+					System.out.println("서버시작");
+				
+					while(true) {
+						DatagramPacket receivePacket = new DatagramPacket(new byte[1024],1024);
+						datagramSocket.receive(receivePacket);
+						String newsKind = new String(receivePacket.getData(),0,receivePacket.getLength(),"UTF-8");
+						
+						SocketAddress socketAddress= receivePacket.getSocketAddress();
+						
+						for(int i=1;i<=10;i++) {
+							String data = newsKind+":뉴스"+i;
+							byte[] bytes=data.getBytes("UTF-8");
+							DatagramPacket sendPacket = new DatagramPacket(bytes,0,bytes.length,socketAddress);
+							datagramSocket.send(sendPacket);
+						}
+					}
+				}catch(IOException e ) {
+					System.out.println("서버"+e.getMessage());
+				}
+				
+			}
+		};
+		thread.start();
+	}
+	public static void stopServer() {
 		
+			datagramSocket.close();
+			System.out.println("서버 종료");
 	}
 }
